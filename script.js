c9d0ebcdf7b36d99674932e35ca55274bbb6184d
Rizkiee <159923656+Rizkiee@users.noreply.github.com>
2026-03-15 04:04:05 +0800

    Update script.js
    biar lebih stabil

--- script.js
+++ script.js
@@ -14,12 +14,27 @@ avatar:"https://unavatar.io/youtube/@SadewaSagara"
 
 }
 
-/* buat card video */
+/* ambil id youtube */
+function getID(url){
+
+const regExp = /(?:youtube\.com\/(?:live\/|watch\?v=)|youtu\.be\/)([^?&]+)/
+
+const match = url.match(regExp)
+
+return match ? match[1] : null
+
+}
+
+/* buat card */
 function createCard(v){
 
 const id = getID(v.url)
-const thumb = `https://img.youtube.com/vi/${id}/hqdefault.jpg`
-const avatar = members[v.member]?.avatar || members["Pandavva"].avatar
+
+if(!id) return ""
+
+const thumb = `https://i.ytimg.com/vi/${id}/hqdefault.jpg`
+
+const avatar = members[v.member]?.avatar || ""
 
 return `
 
@@ -46,24 +61,29 @@ return `
 </div>
 
 `
+
 }
 
 /* render video */
 function render(member="all"){
 
 const content=document.getElementById("content")
+
 content.innerHTML=""
 
 let filtered = videos
 
-if(member!="all")
-filtered = videos.filter(v=>v.member==member)
+if(member!="all"){
+filtered = videos.filter(v=>v.member===member)
+}
 
+/* ambil kategori unik */
 const types=[...new Set(filtered.map(v=>v.type))]
 
 types.forEach(type=>{
 
 const section=document.createElement("div")
+
 section.className="section"
 
 section.innerHTML=`
@@ -80,11 +100,16 @@ content.appendChild(section)
 
 const row=document.getElementById(`row-${type}`)
 
+/* render card */
+let html=""
+
 filtered
-.filter(v=>v.type==type)
+.filter(v=>v.type===type)
 .forEach(v=>{
-row.innerHTML+=createCard(v)
+html+=createCard(v)
 })
+
+row.innerHTML=html
 
 })
 
@@ -94,45 +119,10 @@ row.innerHTML+=createCard(v)
 function renderMembers(){
 
 const bar=document.getElementById("memberBar")
+
 bar.innerHTML=""
 
 bar.innerHTML+=`
 
 <div class="member" onclick="render('all')">
-<img src="https://i.imgur.com/6VBx3io.png">
-<span>All</span>
-</div>
-
-`
-
-for(const key in members){
-
-const m=members[key]
-
-bar.innerHTML+=`
-
-<div class="member" onclick="render('${key}')">
-<img src="${m.avatar}">
-<span>${m.name}</span>
-</div>
-
-`
-
-}
-
-}
-
-/* load json */
-fetch("videos.json")
-.then(res=>res.json())
-.then(data=>{
-
-videos=data
-
-renderMembers()
-render()
-
-})
-
-/* tombol home */
-document.getElementById("homeBtn").onclick=()=>render()
+<img src="https
