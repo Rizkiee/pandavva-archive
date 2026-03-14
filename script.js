6abb4143fb7b47173a305e9a15e215f6cba26b35
Rizkiee <159923656+Rizkiee@users.noreply.github.com>
2026-03-14 23:44:48 +0800

    Update script.js
    script awal

--- script.js
+++ script.js
@@ -1,1 +1,52 @@
+fetch("videos.json")
+.then(response => response.json())
+.then(videos => {
 
+const container = document.getElementById("video-container");
+
+videos.forEach(video => {
+
+let videoId = "";
+
+// ambil video ID dari berbagai format link youtube
+if(video.link.includes("watch?v=")){
+videoId = video.link.split("watch?v=")[1];
+}
+
+else if(video.link.includes("youtu.be/")){
+videoId = video.link.split("youtu.be/")[1];
+}
+
+else if(video.link.includes("live/")){
+videoId = video.link.split("live/")[1];
+}
+
+// bersihkan parameter tambahan
+if(videoId.includes("&")){
+videoId = videoId.split("&")[0];
+}
+
+// buat card video
+const card = document.createElement("div");
+card.className = "card";
+
+card.innerHTML = `
+<a href="${video.link}" target="_blank">
+
+<img class="thumbnail"
+src="https://img.youtube.com/vi/${videoId}/maxresdefault.jpg">
+
+</a>
+
+<h3>${video.title}</h3>
+
+<p class="meta">
+${video.member} • ${video.type} • ${video.format}
+</p>
+`;
+
+container.appendChild(card);
+
+});
+
+});
